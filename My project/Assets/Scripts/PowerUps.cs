574eb2d5d8ce00d0dc05324795e32f5c39a6edaa
JoacoAba <joaco1130@hotmail.com>
2022-08-25 18:18:02 -0300

    Inicial Commit

---
+++ My project/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] CharManager Player;
+    void Start()
+    {
+        Player = GetComponent(Player)();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            Player.Speed();
+
+        }
+    }
+}
